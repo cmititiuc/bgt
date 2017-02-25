59a3b7ce7793609a28eca257e9d343032f72d6d4
Cat <Catalin.Mititiuc@gmail.com>
2017-02-25 10:19:02 -0500

    move view helpers into transaction view

--- web/views/transaction_view.ex
+++ web/views/transaction_view.ex
@@ -1,3 +1,16 @@
 defmodule Bgt.TransactionView do
   use Bgt.Web, :view
+
+  defp time_zone_convert(datetime) do
+    timezone = Timex.Timezone.get("America/New_York", Timex.now)
+    Timex.Timezone.convert(datetime, timezone)
+  end
+
+  def format_date(datetime) do
+    datetime |> time_zone_convert |> Timex.format!("{WDshort}, {Mshort} {D}, {YYYY}")
+  end
+
+  def format_time(datetime) do
+    datetime |> time_zone_convert |> Timex.format!("{h12}:{m} {AM}")
+  end
 end
--- web/web.ex
+++ web/web.ex
@@ -52,19 +52,6 @@ defmodule Bgt.Web do
       import Bgt.Router.Helpers
       import Bgt.ErrorHelpers
       import Bgt.Gettext
-
-      defp time_zone_convert(datetime) do
-        timezone = Timex.Timezone.get("America/New_York", Timex.now)
-        Timex.Timezone.convert(datetime, timezone)
-      end
-
-      def format_date(datetime) do
-        datetime |> time_zone_convert |> Timex.format!("{WDshort}, {Mshort} {D}, {YYYY}")
-      end
-
-      def format_time(datetime) do
-        datetime |> time_zone_convert |> Timex.format!("{h12}:{m} {AM}")
-      end
     end
   end
 
