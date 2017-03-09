a46ab798dacb7422e1b4b9c424a8540d9a81057b
Cat <Catalin.Mititiuc@gmail.com>
2017-03-08 22:44:17 -0500

    render totals

--- web/views/transaction_view.ex
+++ web/views/transaction_view.ex
@@ -37,17 +37,32 @@ defmodule Bgt.TransactionView do
     |> Enum.sort(&(compare(&1.inserted_at, &2.inserted_at)) == -1)
   end
 
-  def list_transactions(transactions, conn, markup \\ [], date \\ nil)
-  def list_transactions([], _, markup, _), do: Phoenix.HTML.raw markup
-  def list_transactions([h | t], conn, markup, date) do
-    new_markup =
-      if date != h.date do
-        {:safe, row_markup} = render "_date_row.html", conn: conn, transaction: h
-        row_markup
-      else
-        {:safe, row_markup} = render "_row.html", conn: conn, transaction: h
-        row_markup
-      end
-    list_transactions(t, conn, [markup|new_markup], h.date)
+  def list_transactions(transactions, conn, markup \\ [], date \\ nil, acc \\ nil)
+  def list_transactions([], _, markup, _, _), do: Phoenix.HTML.raw markup
+  def list_transactions([_], conn, markup, _, acc) do
+    {:safe, new_markup} = render "_total_row.html", total: acc
+    list_transactions([], conn, [markup|new_markup])
+  end
+  def list_transactions([transaction|transactions], conn, markup, date, acc) do
+    if date != transaction.date do
+      {:safe, new_markup} =
+        render "_date_row.html", conn: conn, transaction: transaction, total: acc
+      list_transactions(
+        transactions,
+        conn,
+        [markup|new_markup],
+        transaction.date,
+        Decimal.new(transaction.amount)
+      )
+    else
+      {:safe, new_markup} = render "_row.html", conn: conn, transaction: transaction
+      list_transactions(
+        transactions,
+        conn,
+        [markup|new_markup],
+        transaction.date,
+        Decimal.add(acc, transaction.amount)
+      )
+    end
   end
 end
